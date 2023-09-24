ab838b80761c042052ad310fde9a2b19c51ada26
snoodle <nicholasduncan37@gmail.com>
2023-09-24 16:35:34 +1000

    Updated asset directories + initial fanroom model

--- Assets/Scripts/LightsourceFlicker.cs
+++ Assets/Scripts/LightsourceFlicker.cs
@@ -7,16 +7,23 @@ public class LightsourceFlicker : MonoBehaviour
     [SerializeField] private bool flickerEnabled = true;
     [SerializeField] private Vector2 onRange = new Vector2(1f,5f);
     [SerializeField] private Vector2 offRange = new Vector2(0.1f,0.5f);
+    [SerializeField] private Vector2 intensityRange = new Vector2(1f,6f);
+    [SerializeField] private float standardIntensityVariance = 0.2f;
 
+    private Light lightComponent;
     private bool flickerOn = false;
     private float flickerDelay;
+    private float flickerIntensity;
+    private float flickerIntensityDefault;
 
     //private LightsourceFlicker emitter;
 
-    //private void Start()
-    //{
-    //    emitter = GetComponent<LightsourceFlicker>();
-    //}
+    private void Start()
+    {
+        //emitter = GetComponent<LightsourceFlicker>();
+        flickerIntensityDefault = gameObject.GetComponent<Light>().intensity;
+        lightComponent = gameObject.GetComponent<Light>();
+    }
 
     private void Update()
     {
@@ -30,11 +37,12 @@ public class LightsourceFlicker : MonoBehaviour
         {
             flickerOn = true;
 
-            this.gameObject.GetComponent<Light>().enabled = false;
+            flickerIntensity = Random.Range(intensityRange.x, intensityRange.y);
+            lightComponent.intensity = flickerIntensity;
             flickerDelay = Random.Range(offRange.x, offRange.y);
             yield return new WaitForSeconds(flickerDelay);
 
-            this.gameObject.GetComponent<Light>().enabled = true;
+            lightComponent.intensity = flickerIntensityDefault + Random.Range(-standardIntensityVariance, standardIntensityVariance);
             flickerDelay = Random.Range(onRange.x, onRange.y);
             yield return new WaitForSeconds(flickerDelay);
 
